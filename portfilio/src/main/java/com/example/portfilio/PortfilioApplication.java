525a911648be7ccc235ace9bb60e065c9ced7e56
Mic-User-05 <gevorgyan604@gmail.com>
2018-07-11 12:02:33 +0400

    Adding my portfolio task

---
+++ portfilio/src/main/java/com/example/portfilio/PortfilioApplication.java
@@ -0,0 +1,12 @@
+package com.example.portfilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfilioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfilioApplication.class, args);
+    }
+}
