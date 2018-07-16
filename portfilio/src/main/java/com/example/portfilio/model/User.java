0626756f234a3d767f05ff2c895a45db793c1cb1
Mic-User-05 <gevorgyan604@gmail.com>
2018-07-16 16:01:00 +0400

    Adding

---
+++ portfilio/src/main/java/com/example/portfilio/model/User.java
@@ -0,0 +1,29 @@
+package com.example.portfilio.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String phoneNumber;
+}
